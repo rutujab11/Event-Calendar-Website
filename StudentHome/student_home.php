3831e24c869fe6f0cddc97c5a7da7ed9234f3e2f
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-02-01 14:54:57 +0530

    Create student_home.php

---
+++ StudentHome/student_home.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Home Page</title>
+	<link rel="stylesheet" type="text/css" href="css/student_home.css">
+</head>
+<body>
+	<div class="main">
+		  <div class="logo">
+    <a href="http://localhost/EC/main.php"><img src="images/logo.png"/></a>
+  </div>
+		<ul>
+			<li class="active"> <a href="http://localhost/EC/student_home.php"> HOME </a> </li>
+			<li> <a href="http://localhost/EC/s_about.php"> ABOUT </a> </li>
+			<li> <a href="http://localhost/EC/s_contact.php"> CONTACT </a> </li>
+		</ul>
+	</div>
+	<center>
+		<div class="card-container1">
+			<div class="card1">
+				<div class="front"> <p> SCRIPT </p></div>
+				<a href="http://localhost/EC/view.php?gname='Script'"><div class="back"> <p>TECH GEEKS</p></div></a>
+			</div>
+		</div>
+	</center>
+	<div class="card-container2">
+		<div class="card2">
+			<div class="front"> <p> SWE </p></div>
+			<a href="http://localhost/EC/view.php?gname='SWE'"><div class="back"> <p>WOMEN ENGINEER</p></div></a>
+		</div>
+	</div>
+	<div class="card-container3">
+		<div class="card3">
+			<div class="front"> <p> CSI </p></div>
+			<a href="http://localhost/EC/view.php?gname='CSI'"><div class="back"> <p> Computer Society Of India</p></div></a>
+		</div>
+	</div>
+	<div class="card-container4">
+		<div class="card4">
+			<div class="front"> <p> AAROHAN </p></div>
+			<a href="http://localhost/EC/view.php?gname='Aarohan'"><div class="back"> <p>ART | MUSIC | DANCE</p></div></a>
+		</div>
+	</div>
+	
+		<div class="cc_main">
+			<div class="c_main">
+				<div class="front"><p> Event Calender </p></div>
+				<a href="#"><div class="back"> WTL PROJECT<p></p></div></a>
+			</div>
+		</div>
+	
+	<div class="card-container5">
+		<div class="card5">
+			<div class="front"> <p> P & G CLUB </p></div>
+			<a href="http://localhost/EC/view.php?gname='PG'"><div class="back"> <p>GOVERNMENT | POLITICS</p></div></a>
+		</div>
+	</div>
+	<div class="card-container6">
+		<div class="card6">
+			<div class="front"> <p> E-CELL </p></div>
+			<a href="http://localhost/EC/view.php?gname='E-cell'"><div class="back"> <p>ENTREPRENEURSHIP</p></div></a>
+		</div>
+	</div>
+	<div class="card-container7">
+		<div class="card7">
+			<div class="front"> <p> MIT CULTURAL </p></div>
+			<a href="http://localhost/EC/view.php?gname='Mit Cultural'"><div class="back"> <p>ART | MUSIC | DANCE</p></div></a>
+		</div>
+	</div>
+	<center>
+		<div class="card-container8">
+			<div class="card8">
+				<div class="front"> <p> ANANTRA TANTRA </p></div>
+				<a href="http://localhost/EC/view.php?gname='AT'""><div class="back"> <p> ANANTRA TANTRA </p></div></a>
+			</div>
+		</div>
+	</center>
+	<footer>
+  		Developers:&emsp; <a href="https://www.linkedin.com/in/aditi-chavan-96a055179/">Aditi Chavan</a> &emsp; | &emsp; <a href="https://www.linkedin.com/in/rutuja-bhandigani-758796195/">Rutuja Bhandigani</a> &emsp;
+	</footer>	
+	
+</body>
+</html>
