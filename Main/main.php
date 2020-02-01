b17c1c71c7d853192f221b25563d6538cb9e08d3
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-02-01 14:49:03 +0530

    Add files via upload

---
+++ Main/main.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>MAIN</title>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <div class="logo">
+    <img src="images/logo.png"/>
+  </div>
+  <div class="title">
+    <h1>EVENT CALENDAR</h1>
+  </div>
+  <div class="nav">
+    <ul>
+      <li class="active"><a href="http://localhost/EC/main.php"> HOME </a></li>
+          <li> <a href="http://localhost/EC/m_about.php"> ABOUT </a> </li>
+          <li> <a href="http://localhost/EC/m_contact.php"> CONTACT </a> </li>
+        </ul>
+  </div>
+  
+  <div class="r1">
+
+  <div class="split left">
+    <div class="centered">
+     <a href="http://localhost/EC/student_home.php">
+       <img src="images/student.jpg" alt="Student">
+     </a> 
+      <h2>STUDENT</h2>
+    </div>
+  </div>
+
+  <div class="split right">
+    <div class="centered">
+      <a href="http://localhost/EC/login.php">
+      <img src="images/cr.jpg" alt="Club Representative"></a>
+      <h2>CLUB REPRESENTATIVE</h2>
+    </div>
+  </div>
+  </div>
+
+  <footer>
+      Developers:&emsp; <a href="https://www.linkedin.com/in/aditi-chavan-96a055179/">Aditi Chavan</a> &emsp; | &emsp; <a href="#">Rutuja Bhandigani</a> &emsp;
+  </footer>
+
+</body>
+</html>
