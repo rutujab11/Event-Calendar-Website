2c175f900554db4a465486dffbb650f160a86c69
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-02-01 14:34:47 +0530

    Create cr_home.php

---
+++ CRhome/cr_home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CR HOME</title>
+	<link rel="stylesheet" type="text/css" href="css/cr_home.css">
+</head>
+<body>
+  <div class="logo">
+    <a href="http://localhost/EC/main.php"><img src="images/logo.png"/></a>
+  </div>
+	<div class="title">
+		<h1>EVENT CALENDAR</h1>
+	</div>
+	<div class="nav">
+		<ul>
+	        <li class="active"> <a href="http://localhost/EC/cr_home.php"> HOME </a> </li>
+	        <li> <a href="http://localhost/EC/c_about.php"> ABOUT </a> </li>
+	        <li> <a href="http://localhost/EC/c_contact.php"> CONTACT </a> </li>
+	        <li> <a href="http://localhost/EC/login.php"> SIGN OUT </a> </li>
+      	</ul>
+	</div>
+	
+	<div class="r1">
+			<a href="http://localhost/EC/view_event.php">
+				<button class="button button1">
+					<img src="images/view.jpg" height="25px" width="25px"/>
+					<p>View Events</p>
+				</button>
+			</a>
+			<a href="http://localhost/EC/add_event.php">
+				<button class="button button2">
+					<img src="images/add.png" height="25px" width="25px" />
+					<p>Add Events</p>
+				</button>
+			</a>
+	</div>
+	<div class="r2">
+		
+	</div>
+	<div class="r3">
+  			<a href="http://localhost/EC/update_event.php">
+  				<button class="button button3">
+  					<img src="images/update.png" height="25px" width="25px"/>
+  					<p>Update Events</p>
+  				</button>
+  			</a>
+  			<a href="http://localhost/EC/delete_event.php">
+  				<button class="button button4">
+  					<img src="images/delete.jpg" height="25px" width="25px"/>
+  					<p>Delete Events</p>
+  				</button>
+  			</a>
+	</div>
+	<footer>
+  		Developers:&emsp; <a href="https://www.linkedin.com/in/aditi-chavan-96a055179/">Aditi Chavan</a> &emsp; | &emsp; <a href="https://www.linkedin.com/in/rutuja-bhandigani-758796195/">Rutuja Bhandigani</a> &emsp;
+	</footer>
+</body>
+</html>
