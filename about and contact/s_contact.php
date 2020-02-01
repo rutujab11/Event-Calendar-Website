29171e96a8b2afaf16a82b6fa19a4c2377449cfe
Rutuja Bhandigani <55191928+rutujab11@users.noreply.github.com>
2020-02-01 14:30:50 +0530

    Add files via upload

---
+++ about and contact/s_contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CONTACT</title>
+  <link rel="stylesheet" type="text/css" href="css/anc.css">
+</head>
+<body>
+  <div class="logo">
+    <a href="http://localhost/EC/main.php"><img src="images/logo.png"/></a>
+  </div>
+  <div class="title">
+    <h1>EVENT CALENDAR</h1>
+  </div>
+  <div class="nav">
+    <ul>
+          <li> <a href="http://localhost/EC/student_home.php"> HOME </a> </li>
+          <li> <a href="http://localhost/EC/s_about.php"> ABOUT </a> </li>
+          <li  class="active"> <a href="http://localhost/EC/s_contact.php"> CONTACT </a> </li>
+        </ul>
+  </div>
+  
+  <div class="r1">
+    <center>
+    <h1>CONACT US</h1>
+    <p><h2>For any queries and support mail us at <a href="#">ecadmin@gmail.com</a></h2></p>
+  </center>
+  </div>
+
+  <footer>
+      Developers:&emsp; <a href="https://www.linkedin.com/in/aditi-chavan-96a055179/">Aditi Chavan</a> &emsp; | &emsp; <a href="#">Rutuja Bhandigani</a> &emsp;
+  </footer>
+
+</body>
+</html>
